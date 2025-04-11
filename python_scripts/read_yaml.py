ae3909cf910cd04193303fd44c235e2d68cf6d7b
topanitanw <sassofono@hotmail.com>
2025-04-11 16:19:15 -0500

    print out the yaml read to stdout

--- python_scripts/read_yaml.py
+++ python_scripts/read_yaml.py
@@ -5,7 +5,7 @@ def replace_tabs_in_yaml(file_path):
         content = file.read()
 
     updated_content = content.replace('\t', ' ' * 4) # Replace with 4 spaces or desired indentation
-    print(f"Updated content:\n{updated_content}")
+    # print(f"Updated content:\n{updated_content}")
     with open(file_path, 'w') as file:
         file.write(updated_content)
 
@@ -18,8 +18,26 @@ def read_yaml_file(file_path):
             print(f"Error reading YAML file: {e}")
             return None
 
+def print_yaml(yaml_data, level=0):
+    indent = ' ' * (level * 4)
+    if isinstance(yaml_data, dict):
+        for key, value in yaml_data.items():
+            if isinstance(value, int) \
+               or isinstance(value, str) \
+               or isinstance(value, float):
+                print(f"{indent}{key}: {value}")
+            else:
+                print(f"{indent}{key}:")
+                print_yaml(value, level + 1)
+    elif isinstance(yaml_data, list):
+        for item in yaml_data:
+            print(f"{indent}-")
+            print_yaml(item, level + 1)
+    else:
+        print(f"{indent}{yaml_data}")
+
 # Example usage:
-file_path = 'rsss_config/cache_dm.out'
+file_path = './rsss_config/cache_dm.out'
 replace_tabs_in_yaml(file_path)
 
 with open(file_path, 'r') as file:
@@ -31,4 +49,6 @@ with open(file_path, 'r') as file:
 yaml_data = read_yaml_file(file_path)
 
 if yaml_data:
-    print(yaml_data)
+    # print(yaml_data)
+    print_yaml(yaml_data)
+
